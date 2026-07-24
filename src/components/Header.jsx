f705b216343b635d19d261d73d495b7b63fef4d8
Phuc2606 <phuc26062005@gmail.com>
2026-07-24 09:31:54 +0700

    Update title

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 export default function Header() {
   return (
     <header className="app-header">
-      <h1>My Todo App</h1>
+      <h1>Deploy App</h1>
       <hr className="divider" />
     </header>
   );
